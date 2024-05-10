0708e4113e65be32c96d6aa004ed17d28affd2e9
tycoonlover1359 <6766257+tycoonlover1359@users.noreply.github.com>
2024-05-10 15:22:06 -0700

    Initial commit

---
+++ test/tycoonlover1359-pics.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Tycoonlover1359Pics from '../lib/tycoonlover1359-pics-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tycoonlover1359-pics-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Tycoonlover1359Pics.Tycoonlover1359PicsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
