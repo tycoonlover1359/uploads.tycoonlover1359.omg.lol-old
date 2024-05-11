8f6ce9c26028f19353ec80b5ae55dcf1ff4f5d6d
tycoonlover1359 <6766257+tycoonlover1359@users.noreply.github.com>
2024-05-10 18:10:28 -0700

    Added bundle minification to lambda function

--- lib/tycoonlover1359-pics-stack.ts
+++ lib/tycoonlover1359-pics-stack.ts
@@ -1,27 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import {
-  aws_lambda_nodejs as lambda_nodejs,
-  aws_lambda as lambda,
-  Duration
+    aws_lambda_nodejs as lambda_nodejs,
+    aws_lambda as lambda,
+    Duration
 } from "aws-cdk-lib";
 import { Construct } from 'constructs';
 
 export class Tycoonlover1359PicsStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    const fn = new lambda_nodejs.NodejsFunction(this, "HelloWorldLambda", {
-      entry: "src/HelloWorldLambda/lambda.ts",
-      handler: "index.handler",
-      timeout: Duration.seconds(10)
-    });
+        const fn = new lambda_nodejs.NodejsFunction(this, "HelloWorldLambda", {
+            entry: "src/HelloWorldLambda/lambda.ts",
+            handler: "index.handler",
+            timeout: Duration.seconds(3),
+            bundling: {
+                minify: true
+            }
+        });
 
-    const fnUrl = fn.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.NONE
-    });
+        const fnUrl = fn.addFunctionUrl({
+            authType: lambda.FunctionUrlAuthType.NONE
+        });
 
-    new cdk.CfnOutput(this, "HelloWorldLmabdaFunctionURL", {
-      value: fnUrl.url
-    });
-  }
+        new cdk.CfnOutput(this, "HelloWorldLmabdaFunctionURL", {
+            value: fnUrl.url
+        });
+    }
 }
