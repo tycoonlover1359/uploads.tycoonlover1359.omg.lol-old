3141a4e9a028ef184749a187a7a28cf9f5fcbf76
tycoonlover1359 <6766257+tycoonlover1359@users.noreply.github.com>
2024-05-10 17:53:39 -0700

    Added esbuild building and local running

---
+++ src/run_local.ts
@@ -0,0 +1,7 @@
+import app from "./HelloWorldLambda/app";
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server listening to http://localhost:${port}`);
+});
