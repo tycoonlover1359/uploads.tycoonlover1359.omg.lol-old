437e80e8df962a83ebe7f2b032f5aac4d62d6bc4
tycoonlover1359 <6766257+tycoonlover1359@users.noreply.github.com>
2024-05-16 19:30:47 -0700

    Change base variable `view` to `body`

--- src/server/ClientLambda/src/routes.ts
+++ src/server/ClientLambda/src/routes.ts
@@ -20,7 +20,7 @@ async function render(req: Request, res: Response, view: string, data?: object) 
     if (req.headers["hx-request"]) {
         [err, result] = await renderer.render(view, data);
     } else {
-        [err, result] = await renderer.render("base", { view: (await renderer.render(view, data))[1] });
+        [err, result] = await renderer.render("base", { body: (await renderer.render(view, data))[1] });
     }
     
     if (err) {
@@ -30,39 +30,11 @@ async function render(req: Request, res: Response, view: string, data?: object) 
 }
 
 router.get("/", async (req: Request, res: Response) => {
-
     render(req, res, "home");
-
-    // const view = "home";
-    // let err, result;
-    // if (req.headers["hx-request"]) {
-    //     [err, result] = await renderer.render(view);
-    // } else {
-    //     [err, result] = await renderer.render("index", { view: (await renderer.render(view))[1] });
-    // }
-
-    // if (err) {
-    //     res.status(500);
-    // }
-    // res.send(result);
 });
 
 router.get("/login", async (req: Request, res: Response) => {
-
     render(req, res, "login");
-
-    // const view = "login";
-    // let err, result;
-    // if (req.headers["hx-request"]) {
-    //     [err, result] = await renderer.render(view);
-    // } else {
-    //     [err, result] = await renderer.render("index", { view: (await renderer.render(view))[1] });
-    // }
-
-    // if (err) {
-    //     res.status(500);
-    // }
-    // res.send(result);
 });
 
 export default router;
