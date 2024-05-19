28fb3065082ccae0b5325dc59d0c078da599a1f8
tycoonlover1359 <6766257+tycoonlover1359@users.noreply.github.com>
2024-05-19 13:50:47 -0700

    Added TSDoc comments to `Renderer.ts` and `Errors.ts`

--- src/server/APILambda/src/Controller/Classes/Errors.ts
+++ src/server/APILambda/src/Controller/Classes/Errors.ts
@@ -1,3 +1,6 @@
+/**
+ * An error for `Upload`s that aren't found in DynamoDB
+ */
 class UploadNotFoundError extends Error {}
 
 export { UploadNotFoundError }
--- src/server/APILambda/src/Controller/Classes/Renderer.ts
+++ src/server/APILambda/src/Controller/Classes/Renderer.ts
@@ -2,19 +2,52 @@ import fs from "node:fs/promises";
 import ejs from "ejs";
 import { S3Handler } from "./S3Handler";
 
+/**
+ * Required interface for all template renders
+ */
 export interface Renderer {
+    /**
+     * Renders a view with the provided data
+     * 
+     * @param view The view to render
+     * @param data The data to pass the view
+     * @returns A promose that resolves to an error or the rendered template
+     */
     render(view: string, data?: object): Promise<[Error | null, string]>;
 }
 
+/**
+ * A renderer that uses the filesystem for template storage.
+ */
 export class FileSystemRenderer implements Renderer {
+    /**
+     * The root directory to search for templates
+     */
     public readonly rootDir: string;
+    
+    /**
+     * The file extension that templates use
+     */
     public readonly extension: string;
 
+    /**
+     * Instantiates a new `FileSystemRenderer`
+     * 
+     * @param rootDir The root directory to search for templates
+     * @param extension The file extension that templates use
+     */
     constructor(rootDir: string, extension?: string) {
         this.rootDir = rootDir;
         this.extension = extension || "ejs";
     }
 
+    /**
+     * Renders a view with the provided data
+     * 
+     * @param view The view to render
+     * @param data The data to pass the view
+     * @returns A promise that resolves to an error or the rendered template
+     */
     public async render(view: string, data?: object): Promise<[Error | null, string]> {
         if (!data) {
             data = {};
@@ -37,18 +70,50 @@ export class FileSystemRenderer implements Renderer {
     }
 }
 
-interface TemplateCache {
+/**
+ * An interface for template caches to use
+ */
+export interface TemplateCache {
+    /**
+     * Gets a template from the cache
+     * 
+     * @param itemName The name of the template to get
+     */
     getItem(itemName: string): Promise<[Error | null, string | null]> | [Error | null, string | null];
+
+    /**
+     * Puts a template into the cache
+     * 
+     * @param itemName The name of the template to cache
+     * @param itemValue The template's data
+     */
     setItem(itemName: string, itemValue: string): Promise<Error | void> | Error | void;
 }
 
-class FilesystemTemplateCache implements TemplateCache {
+/**
+ * A filesystem template cache
+ */
+export class FilesystemTemplateCache implements TemplateCache {
+    /**
+     * The cache directory
+     */
     public readonly cacheDir: string;
 
+    /**
+     * Instantiates a new `FilesystemTemplateCache`
+     * 
+     * @param cacheDir The cache directory
+     */
     constructor(cacheDir: string) {
         this.cacheDir = cacheDir;
     }
 
+    /**
+     * Gets a template from the cache
+     * 
+     * @param itemName The template to get
+     * @returns A promise that resolves to an error or the template
+     */
     public async getItem(itemName: string): Promise<[Error | null, string | null]> {
         try {
             const contents = await fs.readFile(`${this.cacheDir}/${itemName}.ejs`, { encoding: "utf8" });
@@ -58,6 +123,13 @@ class FilesystemTemplateCache implements TemplateCache {
         }
     }
 
+    /**
+     * Puts a template into the cache
+     * 
+     * @param itemName The name of the template to cache
+     * @param itemValue The template's data
+     * @returns Whether there was an error putting the data into the cache; or void
+     */
     public async setItem(itemName: string, itemValue: string): Promise<Error | void> {
         try {
             await fs.writeFile(`${this.cacheDir}/${itemName}.ejs`, itemValue, { encoding: "utf8" });
@@ -67,19 +139,60 @@ class FilesystemTemplateCache implements TemplateCache {
     }
 }
 
+/**
+ * Props for creating an `S3Renderer`
+ */
 interface S3RendererProps {
+    /**
+     * The name of the bucket to use
+     */
     bucket?: string;
+
+    /**
+     * The region the bucket
+     */
     bucketRegion?: string;
+
+    /**
+     * The prefix to use for object keys
+     */
     keyPrefix?: string;
+
+    /**
+     * The `S3Handler` to use
+     */
     s3Handler?: S3Handler;
+
+    /**
+     * The `TemplateCache` to use
+     */
     cache?: TemplateCache
 }
 
+/**
+ * A renderer that uses Amazon S3 for template storage
+ */
 export class S3Renderer implements Renderer {
+    /**
+     * The prefix to use for object keys
+     */
     public readonly keyPrefix: string;
+
+    /**
+     * The `S3Handler` to use
+     */
     private readonly s3Client: S3Handler;
+
+    /**
+     * The `TemplateCache` to use
+     */
     private readonly cache: TemplateCache;
 
+    /**
+     * Instantiates a new `S3Renderer`
+     * 
+     * @param props `S3RendererProps` to use for instantiation
+     */
     constructor(props: S3RendererProps) {
         if (props.s3Handler != null) {
             this.s3Client = props.s3Handler;
@@ -90,6 +203,13 @@ export class S3Renderer implements Renderer {
         this.cache = props.cache || new FilesystemTemplateCache("/tmp");
     }
 
+    /**
+     * Renders a view with the provided data
+     * 
+     * @param view The view to render
+     * @param data The data to pass the view
+     * @returns A promise that resolves to an error or the rendered template
+     */
     public async render(view: string, data?: object): Promise<[Error | null, string]> {
         if (!data) {
             data = {};
