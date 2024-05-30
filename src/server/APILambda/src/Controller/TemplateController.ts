222865ab200bee25e72a893aaaa426607e82cd23
tycoonlover1359 <6766257+tycoonlover1359@users.noreply.github.com>
2024-05-30 12:22:54 -0700

    Removed S3Renderer

--- src/server/APILambda/src/Controller/TemplateController.ts
+++ src/server/APILambda/src/Controller/TemplateController.ts
@@ -1,17 +1,10 @@
 import { Request, Response, Router, NextFunction } from "express";
-import { FileSystemRenderer, Renderer, S3Renderer } from "./Classes/Renderer";
+import { FileSystemRenderer, Renderer } from "./Classes/Renderer";
 import { s3Controller } from "./S3Controller";
 
 let renderer: Renderer;
 
-// if (process.env.DEVELOPMENT == "true") {
-    renderer = new FileSystemRenderer("src/View/templates");
-// } else {
-//     renderer = new S3Renderer({
-//         s3Handler: s3Controller,
-//         keyPrefix: "assets/templates"
-//     })
-// }
+renderer = new FileSystemRenderer("src/View/templates");
 
 async function render(req: Request, view: string, data?: object): Promise<[Error | null, string]> {
     if (!data) {
